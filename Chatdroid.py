dcb3716858cbba510a96476c66c3ccbb7adec9bc
D3v31op3r <89334642+D3v31op3r@users.noreply.github.com>
2022-06-15 02:51:15 +0900

    Update Chatdroid.py

--- Chatdroid.py
+++ Chatdroid.py
@@ -178,7 +178,7 @@ async def on_message(message):
         await message.channel.send('시간이 약간 걸릴수 있어요..')
         url = "https://hiyedang.hs.kr/"
 
-        res = requests.get(url)    #학교 급식게시판 파싱
+        res = requests.get(url,timeout = 40)    #학교 급식게시판 파싱
         res.raise_for_status()
         soup = BeautifulSoup(res.text, "lxml") 
 
