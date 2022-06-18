f7c674ca922688301f760568fdb0bb66cfecea4a
D3v31op3r <89334642+D3v31op3r@users.noreply.github.com>
2022-06-18 18:43:41 +0900

    Update Chatdroid.py

--- Chatdroid.py
+++ Chatdroid.py
@@ -42,31 +42,32 @@ async def on_message(message):
       translator = Translator()
       trans1 = translator.translate(text1, src='ko', dest='en')
       embed = discord.Embed(title="한국어를 영어로 번역했어요", color=0xfaf4c0)
-      #await message.channel.send("번역결과: {}" .format(trans1.text))
-    
+      await message.channel.send("번역결과: {}" .format(trans1.text))
+      await message.channel.send(embed=embed)
     
     if message.content.startswith("!한일"):
       text1 = message.content[4:]
       translator = Translator()
       trans2 = translator.translate(text1, src='ko', dest='ja')
       embed = discord.Embed(title="한국어를 일본어로 번역했어요", color=0xfaf4c0)
-      #await message.channel.send("번역결과: {}" .format(trans2.text))
-    
+      await message.channel.send("번역결과: {}" .format(trans2.text))
+      await message.channel.send(embed=embed)
     
     if message.content.startswith("!영한"):
       text1 = message.content[4:]
       translator = Translator()
       trans3 = translator.translate(text1, src='en', dest='ko')
       embed = discord.Embed(title="영어를 한국어로 번역했어요", color=0xfaf4c0)
-      #await message.channel.send("번역결과: {}" .format(trans3.text))
-    
+      await message.channel.send("번역결과: {}" .format(trans3.text))
+      await message.channel.send(embed=embed)
     
     if message.content.startswith("!일한"):
       text1 = message.content[4:]
       translator = Translator()
       trans4 = translator.translate(text1, src='ja', dest='ko')
       embed = discord.Embed(title="일본어를 한국어로 번역했어요" ,color=0xfaf4c0)
-      #await message.channel.send("번역결과: {}" .format(trans4.text))
+      await message.channel.send("번역결과: {}" .format(trans4.text))
+      await message.channel.send(embed=embed)
    
   
     if message.content.startswith('!312안녕'):
