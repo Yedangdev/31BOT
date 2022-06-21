fffc2bca1a9f84d5f26013785fe0941b6011a5e9
D3v31op3r <89334642+D3v31op3r@users.noreply.github.com>
2022-06-21 09:24:27 +0900

    Update Chatdroid.py

--- Chatdroid.py
+++ Chatdroid.py
@@ -224,17 +224,23 @@ async def on_message(message):
     if message.content.startswith("!반란"):
         await message.channel.purge(limit=1)
         nme = message.content[4:]
+        if nme == "지빈" or "최지빈":
+            nme = "아버지한테 어케 욕해"
+            
         be = randint(1,100)
         if 1 <= be < 30:
-            bly = "씨발새끼"
+            bly = "씨발새끼야"
              
 
         if 30 <= be < 60:
-            bly = "병신새끼"
+            bly = "병신새끼야"
               
 
         if 60 <= be < 100:
-            bly = "씹련"
+            bly = "씹련아"
+            
+        #if be == 100:
+            #bly = "불가"
            
         
         await message.channel.send(f"{nme} {bly}")
@@ -268,25 +274,25 @@ async def on_message(message):
         await message.channel.send('저는 파이썬 기반으로 만들어졌어요!')
         
         
-    #if message.content.startswith('!급식'):
-        #await message.channel.send('시간이 약간 걸릴수 있어요..')
-        #url = "https://hiyedang.hs.kr/"
+    if message.content.startswith('!급식'):
+        await message.channel.send('시간이 약간 걸릴수 있어요..')
+        url = "https://hiyedang.hs.kr/"
 
-        #res = requests.get(url,timeout = 40)    #학교 급식게시판 파싱
-        #res.raise_for_status()
-        #soup = BeautifulSoup(res.text, "lxml") 
+        res = requests.get(url,timeout = 30)    #학교 급식게시판 파싱
+        res.raise_for_status()
+        soup = BeautifulSoup(res.text, "lxml") 
 
-        #diet = soup.find_all("div", attrs={"class":"menu"})  #가져올 요소
-        #for diets in diet:
-            #result = diets.get_text() #텍스트만 추출
+        diet = soup.find_all("div", attrs={"class":"menu"})  #가져올 요소
+        for diets in diet:
+            result = diets.get_text() #텍스트만 추출
             
-            #await message.channel.purge(limit=1)
+            await message.channel.purge(limit=1)
             
-        #result = get_diet()
+        result = get_diet()
         #await message.channel.send("어케 알아 씨발")
-        #embed=discord.Embed(color=0xff00, title= "오늘의 급식", description= f"{result}", timestamp=message.created_at)
-        #embed.set_footer(text=message.author, icon_url=message.author.avatar_url)
-        #await message.channel.send(embed=embed)
+        embed=discord.Embed(color=0xff00, title= "오늘의 급식", description= f"{result}", timestamp=message.created_at)
+        embed.set_footer(text=message.author, icon_url=message.author.avatar_url)
+        await message.channel.send(embed=embed)
         
     
     if message.content.startswith ("!청소"):
