9d991d67c50aa5ba59c6d9ca7a2c46f02ddb1336
D3v31op3r <89334642+D3v31op3r@users.noreply.github.com>
2022-07-04 00:24:54 +0900

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -2,18 +2,18 @@ import requests, re
 from bs4 import BeautifulSoup
   
   
-  def get_diet(): 
+ def get_diet(): 
          #await message.channel.send('시간이 약간 걸릴수 있어요..') 
-         url = "https://hiyedang.hs.kr/" 
+  url = "https://hiyedang.hs.kr/" 
 
-         res = requests.get(url,timeout = 40)    #학교 급식게시판 파싱 
-         res.raise_for_status() 
-         soup = BeautifulSoup(res.text, "lxml")  
+  res = requests.get(url,timeout = 40)    #학교 급식게시판 파싱 
+  res.raise_for_status() 
+  soup = BeautifulSoup(res.text, "lxml")  
 
-         diet = soup.find_all("div", attrs={"class":"menu"})  #가져올 요소 
-         for diets in diet: 
+  diet = soup.find_all("div", attrs={"class":"menu"})  #가져올 요소 
+  for diets in diet: 
 
-             result = diets.get_text() #텍스트만 추출
+    result = diets.get_text() #텍스트만 추출
             
             
   def get_UserInfo(soup): #유저 정보 가져오는 코드 (Try Except)
