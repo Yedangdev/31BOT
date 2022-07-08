7beae7a2a451e2d46b69c24b2f319fc560ecdb84
D3v31op3r <89334642+D3v31op3r@users.noreply.github.com>
2022-07-09 00:19:05 +0900

    Update Chatdroid.py

--- Chatdroid.py
+++ Chatdroid.py
@@ -42,7 +42,7 @@ async def on_message(message):
     
     if message.content.startswith("!마법의 소라고둥님"):
         words = ["돼.", "안돼.", "포기해.", "다시 한 번 물어봐.", "허락할게", "당장 시작해.", "나중에 해.", "안.돼.", "하지마.", "그래.", "가만히 있어.", "그것도 안 돼.", "아니.", "응.", "하고 싶은 대로 해.", "그것도 하지마.", "맘대로 해.","꿈도 꾸지 마.","기다려.","왜?."]
-        sora = random.randrange(0,19)
+        sora = randrange(0,19)
         sorare = words[sora]
         
         say = message.content[1:]
