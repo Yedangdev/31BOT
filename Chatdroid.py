dd9796bfb0f46c1e963009a41631797c5e658be1
D3v31op3r <89334642+D3v31op3r@users.noreply.github.com>
2022-07-18 01:50:59 +0900

    Update Chatdroid.py

--- Chatdroid.py
+++ Chatdroid.py
@@ -168,7 +168,7 @@ async def on_message(message):
  
  
             
-        await message.channel.send(f"**python\n{nme} {bly}**")
+        await message.channel.send(f"**{nme} {bly}**")
     
     if message.content.startswith("!한영"):
       text1 = message.content[4:]
@@ -672,7 +672,7 @@ async def on_message(message):
     if message.content.startswith('!전적'):
       
       await message.channel.send("```python\n시간이 조금 걸려요...```")
-      await asyncio.sleep(1)
+      await asyncio.sleep(0.7)
       await message.channel.purge(limit=2)
       
       msg = message.content[4:]
@@ -701,7 +701,7 @@ async def on_message(message):
       img = soup.find("div", attrs={"id":"user-profile-bio-img"}).find("img").get('src') #소환사이미지 가져오기
       
       
-      embed = discord.Embed(title=f"**{msg}**의 전적!🎮", description = f"**<OP.GG 바로가기>**\n__***https://www.op.gg/summoners/kr/{msg}***__", color=0xfaf4c0)
+      embed = discord.Embed(title=f"**{msg}**님의 전적!🎮", description = f"**<OP.GG 바로가기>**\n__***https://www.op.gg/summoners/kr/{msg}***__", color=0xfaf4c0)
       embed.add_field(name="**<솔로랭크>**", value=f"```python\n{soloRankResult}```\n", inline=True)
       embed.add_field(name="**<자유랭크>**", value=f"```python\n{flexRankResult}```", inline=True)
       #embed.add_field(name="전적사이트 바로가기!", value=f"https://www.op.gg/summoners/kr/{msg}", inline=)
