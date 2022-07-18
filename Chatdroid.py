b17bb9c678a8334837e5b51564d03977b12f117f
D3v31op3r <89334642+D3v31op3r@users.noreply.github.com>
2022-07-18 11:07:16 +0900

    Update Chatdroid.py

--- Chatdroid.py
+++ Chatdroid.py
@@ -779,8 +779,8 @@ async def on_message(message):
         rank = soup.find("div", {"class":"profile__tier__icon"}).find("img").get("src")
         rank2 = soup.find("div", {"class":"profile__tier__icon"}).find("img").get("alt")
         embed = discord.Embed(title=f"**{chess}**님의 전적!🎮", description = f"**<Lolchess.gg 바로가기>**\n**https://lolchess.gg/profile/kr/{chess}**", color=0xfaf4c0)
-        embed.add_field(name=f"{rank}", inline=True)
-        embed.add_field(name=f"**{rank2}**", inline=True)
+        embed.add_field(name=f"{rank}", value = "1", inline=True)
+        embed.add_field(name=f"**{rank2}**", value = "1", inline=True)
         await message.channel.send(embed=embed)
         
 
